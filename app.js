7ea79ee3813e4a268ad1c993b4db24018fe40d0a
Andrew Crisp <andrew.crisp@gmail.com>
2020-08-22 20:37:37 -0600

    Add json

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict;'
+console.log('Entering the js file');
+
+
+$(function () {
+    // Do stuff to the DOM
+    $('body').append('<p>Hello World!</p>');
+    let requestURL = 'lookups.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        const lookups = request.response;
+    }
+});
